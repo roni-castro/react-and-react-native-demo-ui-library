abea1ff49a796c4522f041f771b2d76e062811db
Roni Castro <roni.castro@devbase.us>
2022-06-14 18:15:55 -0300

    feat: add color to web button

--- src/components/button/index.web.tsx
+++ src/components/button/index.web.tsx
@@ -2,5 +2,12 @@ import React from 'react';
 import type { ButtonPropsInterface } from './props.interface';
 
 export function Button(props: ButtonPropsInterface) {
-  return <button onClick={props.onClick}>{props.text}</button>;
+  return (
+    <button
+      style={{ backgroundColor: 'blue', color: 'white' }}
+      onClick={props.onClick}
+    >
+      {props.text}
+    </button>
+  );
 }
