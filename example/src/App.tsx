38ef08a5f30a7e0850e9d9b3bc208bd92b38bf01
Roni Castro <roni.castro@devbase.us>
2022-07-04 18:59:35 -0300

    BREAKING CHANGE: remove text props from web button

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,7 +5,7 @@ import { View } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button text="Button" onClick={() => console.log('onClick')} />
+      <Button onClick={() => console.log('onClick')}>Button</Button>
       <Input placeholder="type here" />
     </View>
   );
